f2f0372f02920a6877a0268776f9552e218e7b79
Pedro Palazón Martínez de Alegría <palazon@gmail.com>
2025-08-18 05:52:29 +0200

    refactor(lab2): add example of Transform in UserDTO

--- nestjs-starter-lab-02/src/users/dto/create.users.dto.ts
+++ nestjs-starter-lab-02/src/users/dto/create.users.dto.ts
@@ -4,7 +4,7 @@ import {
   IsNumber,
   ValidateNested,
 } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 
 class AddressDTO {
   @IsString()
@@ -23,4 +23,8 @@ export class UserDTO {
   @ValidateNested()
   @Type(() => AddressDTO)
   address: AddressDTO;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  @Transform((value) => value.toUpperCase())
+  favoriteColor: string;
 }

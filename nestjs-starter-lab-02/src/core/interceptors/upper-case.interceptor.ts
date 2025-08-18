a21dd0076f2b70931d9b1a454f793e896b85ad22
Pedro Palazón Martínez de Alegría <palazon@gmail.com>
2025-08-18 06:20:08 +0200

    feat(lab2): experiment with interceptors

---
+++ nestjs-starter-lab-02/src/core/interceptors/upper-case.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UppercaseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next
+      .handle()
+      .pipe(
+        map((data: string) =>
+          typeof data === 'string' ? data.toUpperCase() : Data,
+        ),
+      );
+  }
+}

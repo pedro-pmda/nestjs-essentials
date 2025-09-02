d91f8ed4d7cafd32fcd61d504f2c1938d5620e5e
Pedro Palazón Martínez de Alegría <palazon@gmail.com>
2025-09-02 07:36:53 +0200

    chore(lab4): 🪛 Setup GraphQL in the project

--- nestjs-starter-lab-04/src/app.module.ts
+++ nestjs-starter-lab-04/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolverr';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  controllers: [],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
